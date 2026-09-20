6ad3633528cbe32427d5cc9616ac7917fbdd4e84
Olga <olgadubinova8@gmail.com>
2026-09-20 22:36:39 +0300

    finish tasks

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,12 +3,16 @@ import { Router } from 'express';
 import {
   registerUserSchema,
   loginUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 import {
   registerUser,
   loginUser,
   logoutUser,
   refreshUserSession,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 
 const router = Router();
@@ -16,4 +20,14 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/refresh', refreshUserSession);
 router.post('/auth/logout', logoutUser);
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 export default router;
